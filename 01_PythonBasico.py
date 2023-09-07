417add6f53c881a4dd5ee81a66aac3ab57bd17f4
Bluseasun <bluseasun@gmail.com>
2023-09-07 16:06:09 -0600

    01_PythonBasico

---
+++ 01_PythonBasico.py
@@ -0,0 +1,42 @@
+
+#----------------------------------
+#  Marisol I. Barba Sánchez
+#----------------------------------
+# Paradigmas de Programación
+# Matemática Algoritmiica 
+# ESFM-IPN      sept-202
+#----------------------------------
+
+''' ESTE ES UN SUPERCOMENTARIO 
+    DE INICIO A NUESTRO RESUMEN
+'''
+#----------------------------------
+# Operaciones básicas
+#----------------------------------
+print (2+3)
+print (2*3)
+print (2/3)
+print (2**10)
+print (2**0.5)     #raiz cuadrada 
+print (10%2)
+print (10%0.1)     #exclusivo en python 
+
+#-----------------------------------------
+# Para saber el tipo de objeto se usa type
+#-----------------------------------------
+t=0
+print(type(t))  # entero 
+t=3.6
+print(type(t))  #real (flotante)
+t=True 
+print(type(t))  #booleano (bool)
+
+#------------------------------------
+# Mensajes a pantalla
+#------------------------------------
+print ("Este es un camando de python.  ",  "Este es otro enunciado. ", t)
+print ('id:  ', 1)
+print ('First Name: ', 'Steve' )
+print ('Last Name: ',  'Jobs')
+:wq
+
